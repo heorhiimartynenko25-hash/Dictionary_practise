9bebdb8a4762f68e1b2efb16865e36831efa58ca
Heorhii <heorhii.martynenko.25@kse.org.ua>
2026-03-18 15:59:15 +0200

    Розширено команду search: тепер можливий пошук окремо за ім'ям та за номером телефону. Додано перевірку додавання номеру телефону: двоє різних людей не можуть мати однаковий номер телефону.

--- phonebook.py
+++ phonebook.py
@@ -5,24 +5,49 @@ while True:
     command = input("enter command: ")
     if command == "exit":
         break
+
     elif command == "add":
-        phonebook.append(
-            {"name" : input("Enter name: "),
-             "phone": input("Enter phone number: ")}
-        )
+        input_name = input("Enter name: ")
+        input_phone = input("Enter phone number: ")
+
+        for human in phonebook:
+            if human['phone'] == input_phone:
+                print(f"{human['name']} already has this number. Maybe, an error occured.")
+                checker = True
+
+        if not(checker):
+            phonebook.append(
+                {"name" : input_name,
+                 "phone": input_phone}
+            )
+        checker = False
+
     elif command == "show":
         for human in phonebook:
             print(human["name"], ' - ', human["phone"])
+
     elif command == "remove":
         for human in phonebook:
             if human['phone'] == input("Enter phone number: "):
                 phonebook.remove(human)
+
     elif command == "search":
-        search_name = input("Enter name: ")
-        for human in phonebook:
-            if human['name'] == search_name:
-                print(f"{human['name']} - {human['phone']}")
-                checker = True
-        if not(checker):
-            print("Not found")
-        checker = False
+        search_type = input("by name / by phone: ")
+        if search_type == 'by name':
+            search_name = input("Enter name: ")
+            for human in phonebook:
+                if human['name'] == search_name:
+                    print(f"{human['name']} - {human['phone']}")
+                    checker = True
+            if not(checker):
+                print("Not found")
+            checker = False
+        elif search_type == 'by phone':
+            search_phone = input("Enter phone number: ")
+            for human in phonebook:
+                if human['phone'] == search_phone:
+                    print(f"{human['name']} - {human['phone']}")
+                    checker = True
+            if not(checker):
+                print("Not found")
+            checker = False
