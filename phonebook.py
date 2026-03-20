f9e8de93e033d3b90b04d51e13d8056b9e8fec18
Heorhii <heorhii.martynenko.25@kse.org.ua>
2026-03-20 19:34:27 +0200

    Додано функції look_for_repetitions_dict - повертає id записів, які містять дані, передані функції is_name_allowed - перевіряє, чи містить ім'я лише символи із дозволеного набору.

--- phonebook.py
+++ phonebook.py
@@ -1,5 +1,30 @@
+def look_for_repetitions_dict(list_of_dicts: list, key_to_compare: str, value) -> list:
+
+    """
+
+    :param list_of_dicts: list of dictionaries to look through
+    :param key_to_compare: key to compare
+    :param value: value to compare
+    :return: empty list if the value is new, list of indexes with same value otherwise
+    """
+    repetitions_id = []
+    for i in range(len(list_of_dicts)):
+        if list_of_dicts[i][key_to_compare] == value:
+            repetitions_id.append(i)
+
+    return repetitions_id
+
+def is_name_allowed(name: str, allowed_symbols) -> bool:
+    for symbol in name:
+        if symbol not in allowed_symbols:
+            return False
+
+    return True
+
+
 phonebook = []
 checker = False
+symbols_phone = '0123456789-'
 
 while True:
     command = input("enter command: ")
